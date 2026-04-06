0026fb6d48bad3a31f3b174d55dc455e2c4f88c8
absalom <absalomsilwal93@gmail.com>
2026-04-06 15:18:52 +1000

    feat(ranking): compute document rank using TF-IDF and probability scoring

--- backend/app/services/feedback_service.py
+++ backend/app/services/feedback_service.py
@@ -1,12 +1,18 @@
 from app.db.redis import redis_client
 from app.models.feedback import Feedback
+from app.helpers.search import tokenize,clean_words
 
 class FeedbackService:
     @staticmethod
     async def record_click(feedback: Feedback):
         # Format query to ensure consistency
-        query = feedback.query.lower().strip()
-        key = f"{query}:{feedback.doc_id}"
-        await redis_client.incr(key)
+        #query = feedback.query.lower().strip()
+        query_tokens = tokenize(feedback.query)
+        query_cleaned = clean_words(query_tokens)
+        query = ' '.join(query_cleaned)
+        document_key = f"{query}:{feedback.doc_id}"
+        await redis_client.incr(document_key)
+        await redis_client.incr(query)
         # Set expiration to 30 days
-        await redis_client.expire(key, 30 * 24 * 60 * 60)
+        await redis_client.expire(document_key, 30 * 24 * 60 * 60)
+        await redis_client.expire(query, 30 * 24 * 60 * 60)
--- backend/app/services/ranking_service.py
+++ backend/app/services/ranking_service.py
@@ -1,10 +1,12 @@
 from typing import List, Dict
-import redis.asyncio as redis
+from app.db.redis import redis_client
 from collections import defaultdict
 
 class RankingService:
     async def ranking(docs,query_tokens,indexed_tokens):
         scores =defaultdict(float)
+        alpha = 0.3   # feedback weight
+        beta = 0.5    # probability weight
         for word in query_tokens:
             if word not in indexed_tokens:
                 continue
@@ -15,6 +17,30 @@ class RankingService:
             for doc_id,tf in posting.items():
                 #summing all tf*idf of doc_id
                 scores[doc_id] += tf * idf
+            query = ' '.join(query_tokens)
+
+            #getting total clicks of the query on all the documents
+            total_query_clicks = await redis_client.get(query)
+            if total_query_clicks is None:
+                total_query_clicks = 0
+            else:
+                total_query_clicks = int(total_query_clicks)
+
+            for doc_id,score in scores.items():
+                query_key = f"{query}:{doc_id}"
+
+                # getting click scores only on that document of that query
+                click_score = await redis_client.get(query_key)
+                if click_score is None:
+                    click_score = 0
+                else:
+                    click_score = int(click_score)
+
+                if total_query_clicks > 0:
+                    click_probability = click_score / total_query_clicks
+                else:
+                    click_probability = 0
+                scores[doc_id] = scores[doc_id] + alpha * click_score + beta * click_probability
 
         ranked = sorted(scores.items(), key=lambda x: x[1], reverse=True)
         return ranked
